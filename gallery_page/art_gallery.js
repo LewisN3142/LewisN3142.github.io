126be3ca66ab3722d79dda444f92fb1f5dedee32
LewisN3142 <135419079+LewisN3142@users.noreply.github.com>
2025-04-29 11:41:46 +0100

    Auto-Generate Filters
     - Added code to auto generate gallery filters based on filters declared in js file.
     - Minor style updates

--- gallery_page/art_gallery.js
+++ gallery_page/art_gallery.js
@@ -1,130 +1,137 @@
-$(document).ready(function () {
-  const gallery_table = [
-    {
-      name: "BITD_Digital",
-      alt_text:
-        "Six figures standing in shadows, with a caption reading Oxventure presents: Blades in the Dark. The four men are wearing suits(one a victorian doctor's garb, one high fashion, and the remaining two, heavy brown jackets over patterned shirts), while one woman wears a hooded cloak to cover her hair and the other wears a boxer's garb and is carrying a fully loaded rucksack. The image is a painting in watercolour and ink.",
-      filter_tags: "gallery_watercolour gallery_pen gallery_fanart",
-      description:
-        'Fan art of the table-top role-playing group <a href="https://www.oxventure.com/" rel="noopener noreferrer" class="external-link under-overlay"> Oxventure</a>\'s playthrough of Blades in the Dark, with each cast member drawn as their character. Based on original designs by <a href="https://www.instagram.com/themarvelousmrc" rel="noopener noreferrer" class="external-link under-overlay"> Colin Craker</a>, with linework completed in pen, colours in watercolor, and lighting effects using Affinity Photo.',
-    },
-    {
-      name: "BITD_Pen",
-      alt_text:
-        "Six figures standing in shadows, with a caption reading Oxventure presents: Blades in the Dark. The four men are wearing suits(one a victorian doctor's garb, one high fashion, and the remaining two, heavy brown jackets over patterned shirts), while one woman wears a hooded cloak to cover her hair and the other wears a boxer's garb and is carrying a fully loaded rucksack. The image is an artwork in pen.",
-      filter_tags: "gallery_pen gallery_fanart",
-      description:
-        'Fan art of the table-top role-playing group <a href="https://www.oxventure.com/" rel="noopener noreferrer" class="external-link under-overlay"> Oxventure</a>\'s playthrough of Blades in the Dark, with each cast member drawn as their character. Based on original designs by <a href="https://www.instagram.com/themarvelousmrc" rel="noopener noreferrer" class="external-link under-overlay"> Colin Craker</a>. This image is of the base pen layer, on textured paper, which was later painted in watercolour.',
-    },
-    {
-      name: "Slaine",
-      alt_text:
-        "A celtic warrior stands at the base of a misty valley, in the centre of a stone circle. He wears a deer-skull helm, a belt with a boar's head embossed in the front, a kilt, and a large wooden cloak and is holding a glowing sword above his head. Shadowy figures emerge from behind the stones.",
-      filter_tags: "gallery_digital gallery_fanart",
-      description:
-        "My entry into the 2000AD Art Stars competition in 2020! Inspired by Clint Langley's design of Sláine, Lord of Misrule, this piece was my first time going from sketch to full render digitally, working mostly in Paint Tool Sai, before moving into Adobe Photoshop to add lighting and colour filters.",
-    },
-    {
-      name: "Foxes_Digital",
-      alt_text:
-        "A humanoid with a fox's face and nine tails stands side-on, holding a glowing crown, but otherwise wreathed in shadow. They are wearing plate armour, save for their head, hands, and tails, and have a pensieve look on their face. The image is a painting in watercolour and ink",
-      filter_tags: "gallery_watercolour gallery_pen gallery_trade",
-      description:
-        'My part of an art-share with the amazing <a href="https://www.instagram.com/flash_psd/" rel="noopener noreferrer" class="external-link under-overlay"> John Gordon</a>, where we each drew a piece based on a list of criteria provided by the other artist! My prompt was "Vulpus Rex" or "King of the Foxes" and the piece consists of a pen outline, watercolour shading, and lighting effects in Adobe Photoshop.',
-    },
-    {
-      name: "Foxes_Watercolour",
-      alt_text:
-        "A humanoid with a fox's face and nine tails stands side-on, holding a glowing crown, but otherwise wreathed in shadow. They are wearing plate armour, save for their head, hands, and tails, and have a pensieve look on their face. The image is an artwork in pen.",
-      filter_tags: "gallery_pen gallery_trade",
-      description:
-        'My part of an art-share with the amazing <a href="https://www.instagram.com/flash_psd/" rel="noopener noreferrer" class="external-link under-overlay"> John Gordon</a>, where we each drew a piece based on a list of criteria provided by the other artist! My prompt was "Vulpus Rex" or "King of the Foxes." This image is of the base pen layer, on textured paper, which was later painted with watercolour.',
-    },
-    {
-      name: "Byrnison",
-      alt_text:
-        "A pencil sketch of a polar bear with a scarred face, standing over a Texan man wearing a cowboy hat and woolen coat, alongside a hare. The man has a short beard and moustache. All three are only shown from the shoulders up.",
-      filter_tags: "gallery_fanart gallery_pencil",
-      description:
-        "A pencil sketch of Lee Scoresby (Lin Manuel Miranda), his daemon the hare Hester, and the panserbjørn Iorek Byrnison from the TV series His Dark Materials, based on the book series of the same name by Philip Pullman. By using a technical pencil and varied line weights, I was able to mimic the textures of materials such as fur and wool.",
-    },
-    {
-      name: "Breakquest_Watercolour",
-      alt_text: "",
-      filter_tags: "gallery_fanart gallery_pen gallery_watercolour",
-      description: "",
-    },
-    {
-      name: "Breakquest_Pen",
-      alt_text: "",
-      filter_tags: "gallery_fanart gallery_pen",
-      description: "",
-    },
-    {
-      name: "Croc_Monochrome",
-      alt_text: "",
-      filter_tags: "gallery_pencil gallery_pen gallery_trade",
-      description: "",
-    },
-    {
-      name: "Croc_Sepia",
-      alt_text: "",
-      filter_tags: "gallery_pencil gallery_pen gallery_trade",
-      description: "",
-    },
-    {
-      name: "Gargoyle",
-      alt_text: "",
-      filter_tags: "gallery_acrylic",
-      description: "",
-    },
-    {
-      name: "Hellblade_Monochrome",
-      alt_text: "",
-      filter_tags: "gallery_pen gallery_fanart",
-      description: "",
-    },
-    {
-      name: "Hellblade_Pen",
-      alt_text: "",
-      filter_tags: "gallery_pen gallery_fanart",
-      description: "",
-    },
-    {
-      name: "Neverwinter_Mice_Digital",
-      alt_text: "",
-      filter_tags: "gallery_pen gallery_fanart gallery_digital",
-      description: "",
-    },
-    {
-      name: "Neverwinter_Mice_Pen",
-      alt_text: "",
-      filter_tags: "gallery_pen gallery_fanart",
-      description: "",
-    },
-    {
-      name: "Jakub",
-      alt_text: "",
-      filter_tags: "gallery_pencil gallery_portrait",
-      description: "",
-    },
-    {
-      name: "Layton_Switch",
-      alt_text: "",
-      filter_tags: "gallery_fanart gallery_digital",
-      description: "",
-    },
-    {
-      name: "Trunchbull",
-      alt_text: "",
-      filter_tags: "gallery_pen gallery_fanart",
-      description: "",
-    },
-  ];
+// filter options should be of the form gallery_Filter-Name-Shown-To-User
 
+const gallery_table = [
+  {
+    name: "BITD_Digital",
+    alt_text:
+      "Six figures standing in shadows, with a caption reading Oxventure presents: Blades in the Dark. The four men are wearing suits(one a victorian doctor's garb, one high fashion, and the remaining two, heavy brown jackets over patterned shirts), while one woman wears a hooded cloak to cover her hair and the other wears a boxer's garb and is carrying a fully loaded rucksack. The image is a painting in watercolour and ink.",
+    filter_tags: "gallery_Watercolour gallery_Pen gallery_Fan-Art",
+    description:
+      'Fan art of the table-top role-playing group <a href="https://www.oxventure.com/" rel="noopener noreferrer" class="external-link under-overlay"> Oxventure</a>\'s playthrough of Blades in the Dark, with each cast member drawn as their character. Based on original designs by <a href="https://www.instagram.com/themarvelousmrc" rel="noopener noreferrer" class="external-link under-overlay"> Colin Craker</a>, with linework completed in pen, colours in watercolor, and lighting effects using Affinity Photo.',
+  },
+  {
+    name: "BITD_Pen",
+    alt_text:
+      "Six figures standing in shadows, with a caption reading Oxventure presents: Blades in the Dark. The four men are wearing suits(one a victorian doctor's garb, one high fashion, and the remaining two, heavy brown jackets over patterned shirts), while one woman wears a hooded cloak to cover her hair and the other wears a boxer's garb and is carrying a fully loaded rucksack. The image is an artwork in pen.",
+    filter_tags: "gallery_Pen gallery_Fan-Art",
+    description:
+      'Fan art of the table-top role-playing group <a href="https://www.oxventure.com/" rel="noopener noreferrer" class="external-link under-overlay"> Oxventure</a>\'s playthrough of Blades in the Dark, with each cast member drawn as their character. Based on original designs by <a href="https://www.instagram.com/themarvelousmrc" rel="noopener noreferrer" class="external-link under-overlay"> Colin Craker</a>. This image is of the base pen layer, on textured paper, which was later painted in watercolour.',
+  },
+  {
+    name: "Slaine",
+    alt_text:
+      "A celtic warrior stands at the base of a misty valley, in the centre of a stone circle. He wears a deer-skull helm, a belt with a boar's head embossed in the front, a kilt, and a large wooden cloak and is holding a glowing sword above his head. Shadowy figures emerge from behind the stones.",
+    filter_tags: "gallery_Digital gallery_Fan-Art",
+    description:
+      "My entry into the 2000AD Art Stars competition in 2020! Inspired by Clint Langley's design of Sláine, Lord of Misrule, this piece was my first time going from sketch to full render digitally, working mostly in Paint Tool Sai, before moving into Adobe Photoshop to add lighting and colour filters.",
+  },
+  {
+    name: "Foxes_Digital",
+    alt_text:
+      "A humanoid with a fox's face and nine tails stands side-on, holding a glowing crown, but otherwise wreathed in shadow. They are wearing plate armour, save for their head, hands, and tails, and have a pensieve look on their face. The image is a painting in watercolour and ink",
+    filter_tags: "gallery_Watercolour gallery_Pen gallery_Art-Trade",
+    description:
+      'My part of an art-share with the amazing <a href="https://www.instagram.com/flash_psd/" rel="noopener noreferrer" class="external-link under-overlay"> John Gordon</a>, where we each drew a piece based on a list of criteria provided by the other artist! My prompt was "Vulpus Rex" or "King of the Foxes" and the piece consists of a pen outline, watercolour shading, and lighting effects in Adobe Photoshop.',
+  },
+  {
+    name: "Foxes_Watercolour",
+    alt_text:
+      "A humanoid with a fox's face and nine tails stands side-on, holding a glowing crown, but otherwise wreathed in shadow. They are wearing plate armour, save for their head, hands, and tails, and have a pensieve look on their face. The image is an artwork in pen.",
+    filter_tags: "gallery_Pen gallery_Art-Trade",
+    description:
+      'My part of an art-share with the amazing <a href="https://www.instagram.com/flash_psd/" rel="noopener noreferrer" class="external-link under-overlay"> John Gordon</a>, where we each drew a piece based on a list of criteria provided by the other artist! My prompt was "Vulpus Rex" or "King of the Foxes." This image is of the base pen layer, on textured paper, which was later painted with watercolour.',
+  },
+  {
+    name: "Byrnison",
+    alt_text:
+      "A pencil sketch of a polar bear with a scarred face, standing over a Texan man wearing a cowboy hat and woolen coat, alongside a hare. The man has a short beard and moustache. All three are only shown from the shoulders up.",
+    filter_tags: "gallery_Fan-Art gallery_Pencil",
+    description:
+      "A pencil sketch of Lee Scoresby (Lin Manuel Miranda), his daemon the hare Hester, and the panserbjørn Iorek Byrnison from the TV series His Dark Materials, based on the book series of the same name by Philip Pullman. By using a technical pencil and varied line weights, I was able to mimic the textures of materials such as fur and wool.",
+  },
+  {
+    name: "Breakquest_Watercolour",
+    alt_text: "",
+    filter_tags: "gallery_Fan-Art gallery_Pen gallery_Watercolour",
+    description: "",
+  },
+  {
+    name: "Breakquest_Pen",
+    alt_text: "",
+    filter_tags: "gallery_Fan-Art gallery_Pen",
+    description: "",
+  },
+  {
+    name: "Croc_Monochrome",
+    alt_text: "",
+    filter_tags: "gallery_Pencil gallery_Pen gallery_Art-Trade",
+    description: "",
+  },
+  {
+    name: "Croc_Sepia",
+    alt_text: "",
+    filter_tags: "gallery_Pencil gallery_Pen gallery_Art-Trade",
+    description: "",
+  },
+  {
+    name: "Gargoyle",
+    alt_text: "",
+    filter_tags: "gallery_Acrylic",
+    description: "",
+  },
+  {
+    name: "Hellblade_Monochrome",
+    alt_text: "",
+    filter_tags: "gallery_Pen gallery_Fan-Art",
+    description: "",
+  },
+  {
+    name: "Hellblade_Pen",
+    alt_text: "",
+    filter_tags: "gallery_Pen gallery_Fan-Art",
+    description: "",
+  },
+  {
+    name: "Neverwinter_Mice_Digital",
+    alt_text: "",
+    filter_tags: "gallery_Pen gallery_Fan-Art gallery_Digital",
+    description: "",
+  },
+  {
+    name: "Neverwinter_Mice_Pen",
+    alt_text: "",
+    filter_tags: "gallery_Pen gallery_Fan-Art",
+    description: "",
+  },
+  {
+    name: "Jakub",
+    alt_text: "",
+    filter_tags: "gallery_Pencil gallery_Portrait",
+    description: "",
+  },
+  {
+    name: "Layton_Switch",
+    alt_text: "",
+    filter_tags: "gallery_Fan-Art gallery_Digital",
+    description: "",
+  },
+  {
+    name: "Trunchbull",
+    alt_text: "",
+    filter_tags: "gallery_Pen gallery_Fan-Art",
+    description: "",
+  },
+];
+
+$(document).ready(function () {
   // Loop through table, generate map, generate html for page
   var description_map = new Map();
+  var filter_set = new Set();
+  var masonryContainer = document.getElementById("masonry-container");
+  var filterColumns = document.getElementById("filter-columns");
   gallery_table.forEach(addGalleryImage);
+  var filter_array = Array.from(filter_set).sort();
+  filter_array.forEach(addFilterButton);
 
   function createElement(type, props) {
     var element = document.createElement(type);
@@ -160,10 +167,38 @@ $(document).ready(function () {
       alt: item.alt_text,
     });
 
+    item.filter_tags.split(" ").forEach(function (filterName) {
+      filter_set.add(filterName);
+    });
+
     description_map.set(item.name, item.description);
 
     newButton.appendChild(newImage);
-    document.getElementById("masonry-container").appendChild(newButton);
+    masonryContainer.appendChild(newButton);
+  }
+
+  function addFilterButton(filterName) {
+    // Construct id, aria label, and inner html from filterName
+    var filterNameGalleryless = filterName.split("_")[1];
+    var filterNameWords = filterNameGalleryless.replace(/-/g, " ");
+
+    var newIcon = createElement("i", {
+      class: "abstract-icon icon-checkbox-unchecked",
+    });
+
+    var newButton = createElement("button", {
+      class:
+        "modal-button under-modal-overlay under-overlay gallery-filter-button",
+      id: filterName,
+      type: "checkbox",
+      "aria-controls": "masonry-container",
+      "aria-label": "filters gallery to show only " + filterNameWords,
+    });
+
+    newButton.appendChild(newIcon);
+    newButton.innerHTML += filterNameWords;
+    filterColumns.appendChild(newButton);
+    filterColumns.appendChild(createElement("br", {}));
   }
 
   $(".close-gallery-modal").click(function () {
